52e84986da88e50555370b0009ec43aad19c6049
prjacobson <peter.robert.jacobson@proton.me>
2026-01-07 20:43:00 -0500

    palettes as class, no theme function yet

--- utils/palettes.py
+++ utils/palettes.py
@@ -14,6 +14,51 @@ class palette:
     def __init__(self,primary_colors, scheme: str):
         self.primary_colors = primary_colors # starting colors
         self.scheme = scheme # color scheme method used
+        self.extra_colors = [] # No extra colors at start 
+
+    # Expand palette
+    expansion_methods = {
+            "unidirectional" : lambda c, n, p: c.N_fudge(N=n, param=p),
+            "symmetric" : lambda c, n, p: c.sym_fudge(N=n, param=p)
+    }
+    def expand(self, color=None,N=1,method="unidirectional",param="h"):
+        if color is None:
+            color = choice(self.primary_colors)
+        # Feels sketchy having no return but okay 
+        self.extra_colors.extend(self.expansion_methods[method](color,N,param))
+    # add extra colors to size N
+    def expand_to_N(self, size=5):
+        params = ["h", "s", "l"]
+        methods = list(self.expansion_methods.keys())
+        # Only expand off of original colors
+        to_add = size-len(self.primary_colors)
+        # Check
+        if to_add <= 0:
+            return 
+        # If only need one, have to expand unidirectionally 
+        if to_add == 1:
+            param = choice(params)
+            self.expand(color=None,N=1, method="unidirectional", param=param)
+            return 
+        for i in range(len(self.primary_colors)):
+            if i+1 == len(self.primary_colors): add_amount = to_add # last color gets all colors
+            else: add_amount = choice(list(range(to_add+1)))
+            to_add = to_add-add_amount
+            param = choice(params)
+            # Check if symmetric makes sense, choose method
+            if add_amount == 0: 
+                continue
+            if add_amount%2 == 0:
+                method=choice(methods)
+            else: method="unidirectional"
+            if method == "symmetric": add_amount = int(add_amount/2)
+            # add to palette
+            self.expand(color=self.primary_colors[i],N=add_amount,method=method,param=param)
+    def preview(self,verbose=False):
+        print("Primary colors:")
+        for i in self.primary_colors: i.preview(verbose=verbose)
+        print("Extra colors:")
+        for i in self.extra_colors: i.preview(verbose=verbose)
 
 # All palette schemes    
 palette_schemes = {
@@ -40,7 +85,6 @@ def generate_palette(base: HSL, scheme: str):
     except KeyError:
          raise ValueError(f"Unknown palette scheme: {scheme}")
 
-# Colorscheme palettes
 # Get random palette
 def get_palette(base=None,scheme=None,weighted=False,preferential=False):
     if base is None:
@@ -56,66 +100,9 @@ def get_palette(base=None,scheme=None,weighted=False,preferential=False):
     palette = generate_palette(base,scheme)
     return palette
 
-# Expanding colorscheme palettes with fudging
-# Expand to N total colors
-expansion_methods = {
-        "unidirectional" : lambda c, n, p: c.N_fudge(N=n, param=p),
-        "symmetric" : lambda c, n, p: c.sym_fudge(N=n, param=p)
-}
-# Function to add colors to a palette
-def expand_palette(pallete, color=None,N=1, method="unidirectional", param="h"):
-    if color is None:
-        color = choice(pallete)
-    return expansion_methods[method](color,N,param)
-# Function to get a palette to a certain size
-def bigger_palette(palette, size=5):
-    params = ["h", "s", "l"]
-    methods = list(expansion_methods.keys())
-    # Only expand off of original colors
-    orig_palette = palette.copy()
-    new_palette = []
-    new_palette.extend(orig_palette)
-    to_add = size-len(orig_palette)
-    # Check
-    if to_add <= 0:
-        return new_palette
-    # If only need one, have to expand unidirectionally 
-    if to_add == 1:
-        param = choice(params)
-        new_palette.extend(expand_palette(new_palette, N=1, method="unidirectional", param=param))
-        return new_palette
-    for i in range(len(orig_palette)):
-        if i+1 == len(orig_palette): add_amount = to_add
-        else: add_amount = choice(list(range(to_add+1)))
-        to_add = to_add-add_amount
-        param = choice(params)
-        # Check if symmetric makes sense, choose method
-        if add_amount == 0: 
-            continue
-        if add_amount%2 == 0:
-            method=choice(methods)
-        else: method="unidirectional"
-        if method == "symmetric": add_amount = int(add_amount/2)
-        # add to palette
-        new_palette.extend(expand_palette(new_palette, color=orig_palette[i],N=add_amount, method=method, param=param))
-    return new_palette
 
 # Function to get a random palette of size N
-def random_N_palette(N=5,preview=False,base=None,method=None):
-    base_palette = random_palette(base=base,method=method)
-    N_palette = bigger_palette(base_palette,size=N)
-    if preview:
-        for i in N_palette: i.preview()
-    return N_palette
-def weighted_N_palette(N=5,preview=False,base=None,method=None):
-    base_palette = weighted_palette(base=base,method=method)
-    N_palette = bigger_palette(base_palette,size=N)
-    if preview:
-        for i in N_palette: i.preview()
-    return N_palette
-def preferential_N_palette(N=5,preview=False,base=None,method=None):
-    base_palette = preferential_palette(base=base,method=method)
-    N_palette = bigger_palette(base_palette, size=N)
-    if preview:
-        for i in N_palette: i.preview()
-    return N_palette
+def random_N_palette(N=5,preview=False,base=None,scheme=None,weighted=False,preferential=False):
+    pal = get_palette(base=base,scheme=scheme,weighted=weighted,preferential=preferential)
+    pal.expand_to_N(N)
+    return pal
